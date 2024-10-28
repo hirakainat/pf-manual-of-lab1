b1183a917083cdac6520e6197088ebf4832b3506
hirakainat <realhirakhalid@gmail.com>
2024-10-28 09:45:00 -0700

    Add files via upload

---
+++ lab1 pf solution.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+using namespace std;
+int main()
+
+{
+
+	//cout << "p\tr\to\tg\tr\ta\tm\tm\ti\tn\tg\n i\ts\n f\tu\tn\n" << endl;
+	//cout << "hello!welcome in the world of programming\n";
+	//int num1 = 0;
+	//int num2 = 0;
+	//int product = 0; 
+	//cout << "enter first number:" << endl; 
+	//cin >> num1;
+	//cout << "enter second number:" << endl;
+	//cin >> num2;
+
+	//product= num1 * num2;
+		//cout << "the product is:" << product;
+	//int i = 0,var = 5;
+	//i = i + 1;
+	//cout << var << "*" << i << "\n";
+	//i = i + 1;
+	//cout << var << "*" << i << "\n";
+	//i = i + 1;
+	//cout << var << "*" << i << "\n";
+	//i = i + 1;
+
+	//cout << var << "*" << i << "\n";
+	//i = i + 1;
+	//cout << var << "*" << i << "\n";
+	//i = i + 1;
+	//cout << var << "*" << i << "\n";
+	//i = i + 1;
+	//cout << var << "*" << i << "\n";
+	//i = i + 1;
+	//cout << var << "*" << i << "\n";
+	//i = i + 1;
+	//cout << var << "*" << i << "\n";
+	//i = i + 1;
+	//cout << var << "*" << i << "\n";
+	//i = i + 1;
+	//cout << var << "*" << i << "\n";
+
+	
+//int num1;
+	//int num2;
+	//int sum = 0, product = 0, subtract = 0, divide = 0;
+	//cout << "enter first number " << endl;
+	//cin >> num1;
+		//cout << "enter second number" << endl;
+		//cin >> num2;
+		//sum = num1 + num2;
+	//product = num1*num2;
+	//subtract = num1 - num2;
+	//divide = num1 / num2;
+	//cout << "the sum is " << sum << endl;
+	//cout << "the product is" << product << endl;
+	//cout << "the subtract is" << subtract << endl;
+	//cout << "the divide is" << divide << endl;
+	system("Pause");
+	return 0;
+}
+
